b5680fba6fb38441b02905646ba86ea0d7141247
Flavia <flaviamatta90@gmail.com>
2020-10-26 16:20:44 +0100

    with database

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,5 +8,16 @@
 </head>
 <body>
     <h1>Ciao</h1>
+    <table>
+        @foreach ($students as $student)
+            <tr>
+            <td>{{$student->name}}</td>
+            <td>{{$student->lastname}}</td>
+            <td>{{$student->gender}}</td>
+            <td>{{$student->date_of_birth}}</td>
+
+            </tr>
+        @endforeach
+    </table>
 </body>
 </html>
