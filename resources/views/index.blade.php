d1f9cce1d874f189489b7482ff8f590877aa946e
Flavia <flaviamatta90@gmail.com>
2020-10-26 16:42:52 +0100

    try to scss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>STUDENTS</title>
 </head>
 <body>
-    <h1>Ciao</h1>
+    <h1>ELENCO STUDENTI</h1>
     <table>
         @foreach ($students as $student)
             <tr>
